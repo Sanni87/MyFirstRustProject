8365cf36cee78d4dd3b8e7731cc1f7973faef862
Sanni87 <asier.sannicolas@gmail.com>
2022-12-27 14:02:14 +0100

    add very basic mod

---
+++ src/garden/vegetables.rs
@@ -0,0 +1,6 @@
+#[derive(Debug)]
+pub struct Asparagus {
+    pub name: String,
+    pub age: u8,
+    pub is_green: bool
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use crate::garden::vegetables::Asparagus;
+
+pub mod garden;
+
 fn main() {
     let number1: i32;
     let number2: i32;
@@ -24,6 +28,7 @@ fn main() {
 
     println!("{}", input_text);
 
+    //This works because concat_strings picks params borrowing them (with &)
     another_text = String::from("anotherText");
     input_text = concat_strings(&input_text, &another_text);
 
@@ -36,10 +41,15 @@ fn main() {
 
     println!("{}", input_text);
 
+    //This works because calculate_length picks param borrowing it (with &)
+    //OtherWhise next println!("{}", input_text); will have an error
     let length = calculate_length(&input_text);
     println!("length: {}", length);
 
     println!("{}", input_text);
+
+    //Example importing module
+    print_vegetable();
 }
 
 
@@ -59,4 +69,9 @@ fn concat_strings(string1: &String, string2: &String) -> String {
 
 fn calculate_length(s: &String) -> usize {
     return s.len();
+}
+
+fn print_vegetable() {
+    let plant = Asparagus {name: "my asparagus".to_string(), age: 2, is_green: true};
+    println!("I'm growing {:?}!", plant); //{:?} means print "plant" as string on debug mode. Otherwhise is not printable
 }
